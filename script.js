0b843a3a9aac60e3e4e611637329c2df1281fe8f
dev-salih <salihmu79@gmail.com>
2026-04-20 09:17:59 +0300

    selectAnsweris declared

--- script.js
+++ script.js
@@ -76,6 +76,14 @@ function resetState(){
     }
 }
 
-
+    function selectAnswer(e) {
+        const selectedBtn = e.target;
+        const isCorrect = selectedBtn.dataset.correct === "true";
+        if(isCorrect){
+            selectedBtn.classList.add("correct");
+        } else{
+            selectedBtn.classList.add("incorrect");
+        }
+    }
 
 startQuiz();
